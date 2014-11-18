1e737eda1114e1f789697af632dd2fcd493e5c37
Chiran Agalawatte <cba_44@yahoo.com>
2014-11-18 06:29:15 +0530

    Update Iplist.java
    Adding date and time to array list to determine the time for 5 unsuccesful attempts.
    Adding to package

--- Iplist.java
+++ Iplist.java
@@ -1,14 +1,38 @@
 //Program 1
 
+package ssh;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Iplist {
     
         
     private String ipaddress;
     private int count = 1;
+    private Date date;
     
-    public Iplist(String ipaddress){
+    
+    public Iplist(String ipaddress,String str1,String str2,String str3){
         
         this.ipaddress = ipaddress;
+        
+        String str = str1 + "-" + str2 + " "+str3;
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM-dd hh:mm:ss");    //http://stackoverflow.com/questions/18604408/convert-java-string-to-time-not-date
+        Date date = null;
+        
+        try {
+            
+            date = sdf.parse(str);
+            
+        } catch (Exception ex) {
+            
+            System.out.println(ex);
+            
+        }
+        
+        this.date = date;   //To check the time for 5 unsuccessful attempts
         
     }
     
@@ -31,3 +55,5 @@ public class Iplist {
     }
     
 }
+
+
