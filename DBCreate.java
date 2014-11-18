d1b340e95d40191bdbeadff97872560cf243f132
Chiran Agalawatte <cba_44@yahoo.com>
2014-11-18 06:18:40 +0530

    Update DBCreate.java
    Adding to ssh package
    Now the program waits until apache web server and mysql server starts

--- DBCreate.java
+++ DBCreate.java
@@ -1,6 +1,9 @@
 //Program 1
 
+package ssh;
+
 import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 public class DBCreate {
     
@@ -11,44 +14,50 @@ public class DBCreate {
     
     private final String pw = "mysql";
     
-    public DBCreate(){
+    public DBCreate() {
         
-        try{
-            
-            Class.forName("com.mysql.jdbc.Driver");
-            
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306","root",pw);
-            st = con.createStatement();
-            
-            query = "CREATE DATABASE IF NOT EXISTS sshblock";     //Creating DB if doesn't exist      
-            st.executeUpdate(query);
-            
-            query = "USE sshblock";
-            st.executeUpdate(query);
-            
-            //Creatingtable if doesn't exist
-            
-            query = "CREATE TABLE IF NOT EXISTS blockedIP(\n" +
-                    "    IPAddress VARCHAR (15) PRIMARY KEY,\n" +
-                    "    BlockedDate DATE NOT NULL,\n" +
-                    "    BlockedTime TIME NOT NULL,\n" +
-                    "    ReleaseDate DATE NOT NULL,\n" +
-                    "    ReleaseTime TIME NOT NULL\n" +
-                    ")";
+        while (true) {
+        
+            try{
 
-            st.executeUpdate(query);
-            
-            query = "SET global max_connections = 100000";            
-            st.executeUpdate(query);
+                Class.forName("com.mysql.jdbc.Driver");
 
-            
-            
-        }catch(Exception ex){
-            
-            System.out.println(ex);
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306","root",pw);System.out.println("connection is "+st);
+                st = con.createStatement();
+
+                query = "CREATE DATABASE IF NOT EXISTS sshblock";     //Creating DB if doesn't exist      
+                st.executeUpdate(query);
+
+                query = "USE sshblock";
+                st.executeUpdate(query);
+
+                //Creating table if doesn't exist
+
+                query = "CREATE TABLE IF NOT EXISTS blockedIP(\n" +
+                        "    IPAddress VARCHAR (15) PRIMARY KEY,\n" +
+                        "    BlockedDate DATE NOT NULL,\n" +
+                        "    BlockedTime TIME NOT NULL,\n" +
+                        "    ReleaseDate DATE NOT NULL,\n" +
+                        "    ReleaseTime TIME NOT NULL\n" +
+                        ")";
+
+                st.executeUpdate(query);
+
+                query = "SET global max_connections = 100000";            
+                st.executeUpdate(query);
+
+                break;
+
+            }catch(Exception ex){
+                
+                //Process waits 1 second until apache web server and mysql server starts
+                //System.out.println(ex);
+
+            }
             
         }
         
     }
     
 }
+
